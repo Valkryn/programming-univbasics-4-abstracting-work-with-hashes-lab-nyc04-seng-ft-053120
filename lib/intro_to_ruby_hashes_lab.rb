f06e1c72cf3f5dda1635902142caf5ac3e127907
Calvin Gutierrez <calvinraphaelg@gmail.com>
2020-06-04 14:21:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -2,3 +2,17 @@ def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   { key => value}
 end
+
+def read_from_hash(hash, key)
+  # return the correct value using the hash and key parameters
+  hash[key]
+end
+
+def update_counting_hash(hash, key)
+  
+  if hash[key]
+    hash[key] += 1
+  else
+    hash[key] = 1
+  end
+end
